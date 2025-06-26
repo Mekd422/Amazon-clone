567efa53d19f2183d1b5a9b5f6546c81064af2fd
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-06-26 19:08:50 +0300

    payments  page done

---
+++ src/pages/Payment/Payment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import LayOut from '../../components/LayOut/LayOut'
+
+export default function Payment() {
+  return (
+    <LayOut>
+                
+        </LayOut>
+  )
+}
