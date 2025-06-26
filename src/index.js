7665a2a1f820c5fdce7a4f782506ddb827fdce30
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-06-26 19:14:31 +0300

    b-d

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Dataprovider } from './components/Dataprovider/Dataprovider';
+import {initialState, reducer} from './Utility/reducer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Dataprovider reducer={reducer} initialState={initialState}>
     <App />
+
+    </Dataprovider>
+    
   </React.StrictMode>
 );
 
