e1a6feaaf1e1c9df80a57e74544802c6193a3ec2
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-07-09 00:43:05 +0300

    payment page

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import classes from './Payment.module.css'
+import { DataContext } from '../../components/Dataprovider/Dataprovider';
+
 
 import LayOut from '../../components/LayOut/LayOut'
 
 export default function Payment() {
+
+  const [{basket}] = useContext(DataContext);
+
+  const totalItem = basket?.reduce((amount, item) =>{
+        return item.amount + amount
+    }, 0);
   return (
     <LayOut>
+      {/*header */}
+      <div className={classes.payment__header}>Checkout ({totalItem}) items</div>
+      {/*payment method */}
+      <section className={classes.payment}>
+        {/*address */}
+        <div>
+          <h3>Delivery Address</h3>
+          <div>
+            <div>abe@email.com</div>
+            <div>123 react</div>
+            <div>chicago</div>
+          </div>
+        </div>
+        <hr />
+
+        {/*product */}
+        <div></div>
+        <hr/>
+
+        {/*card form */}
+        <div></div>
+      </section>
                 
         </LayOut>
-  )
+  );
 }
