9ccb3a3d78004add9dc58a512848611b64db5475
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-07-09 13:32:51 +0300

    route for payment

--- src/Router.jsx
+++ src/Router.jsx
@@ -7,6 +7,10 @@ import Orders from './pages/Orders/Orders'
 import Cart from './pages/Cart/Cart'
 import Results from './pages/Results/Results'
 import ProductDetail from './pages/ProductDetail/ProductDetail'
+import {CheckoutProvider, Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51RhqL9R2sCiX9W8h1Y8rv8crUAV5BOuaZT6XTswuvzb3S1pdvRUN7jdovMdbroaAEeFbLBXPphDD828eIcI4LL360077TZb2rI');
 
 export default function Routing() {
   return (
@@ -14,7 +18,11 @@ export default function Routing() {
         <Routes>
             <Route path="/" element={<Landing/>}></Route>
             <Route path="/auth" element={<Auth/>}></Route>
-            <Route path="/payments" element={<Payment/>}></Route>
+            <Route path="/payments" element={
+              <Elements stripe={stripePromise}>
+                <Payment/>
+              </Elements>
+            }/>
             <Route path="/orders" element={<Orders/>}></Route>
             <Route path="/cart" element={<Cart/>}></Route>
             <Route path="/category/:categoryName" element={<Results/>}></Route>
